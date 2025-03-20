a7c78edc0e36eee71d159d146ffd44b085bdffb1
blushell <micheal.jones89@gmail.com>
2025-03-20 10:15:21 -0400

    redirect

--- redirect.js
+++ redirect.js
@@ -5,38 +5,23 @@ function checkProductInfoAndUpdateCTA() {
 	);
 	const ctaElement = document.querySelector('.product-info__cta');
 	const pricesElement = document.querySelector('.product-info__prices');
-	const moreinfoElement = document.querySelector(
-		'.product-info__additional-info'
-	);
 
 	if (titleElement && titleElement.textContent.includes('on Steam')) {
 		if (pricesElement) {
 			pricesElement.style.display = 'none';
 		}
-		const moreinfoText = moreinfoElement ? moreinfoElement.textContent : '';
+		const descriptionText = descriptionElement
+			? descriptionElement.textContent
+			: '';
 
 		switch (true) {
-			case moreinfoText.includes('AMBERISLESTEAM'):
+			case descriptionText.includes('AMBERISLESTEAM'):
 				if (ctaElement) {
 					ctaElement.innerHTML = `
                         <div class="hero__cta-container hero__cta-container--center">
                             <div class="hero__cta hero__cta--primary">
                                 <a href="https://store.steampowered.com/app/1663040/Amber_Isle/" class="button button--primary button--expand">
                                     Buy now on Steam
-                                </a>
-                            </div>
-                        </div>
-                    `;
-				}
-				break;
-
-			case moreinfoText.includes('AMBERISLESWITCH'):
-				if (ctaElement) {
-					ctaElement.innerHTML = `
-                        <div class="hero__cta-container hero__cta-container--center">
-                            <div class="hero__cta hero__cta--primary">
-                                <a href="https://switch.link/" class="button button--primary button--expand">
-                                    Buy now on Nintendo eShop
                                 </a>
                             </div>
                         </div>
