46bc2d59fa0548a6f181830e9685400b4977827e
blushell <micheal.jones89@gmail.com>
2025-03-21 16:47:23 -0400

    redirect

--- redirect.js
+++ redirect.js
@@ -1,28 +1,25 @@
 function checkProductInfoAndUpdateCTA() {
 	const titleElement = document.querySelector('.product-info__title');
-	const descriptionElement = document.querySelector(
-		'.product-info__description'
-	);
 	const ctaElement = document.querySelector('.product-info__cta');
 	const pricesElement = document.querySelector('.product-info__prices');
 	const moreinfoElement = document.querySelector(
 		'.product-info__additional-info'
 	);
 
-	if (titleElement && titleElement.textContent.includes('on Steam')) {
-		if (pricesElement) {
+	if (titleElement && titleElement.textContent.includes('Widget')) {
+		/* if (pricesElement) {
 			pricesElement.style.display = 'none';
-		}
+		} */
 		const moreinfoText = moreinfoElement ? moreinfoElement.textContent : '';
 
 		switch (true) {
-			case moreinfoText.includes('AMBERISLESTEAM'):
+			case moreinfoText.includes('CLOCKWIDGET'):
 				if (ctaElement) {
 					ctaElement.innerHTML = `
                         <div class="hero__cta-container hero__cta-container--center">
                             <div class="hero__cta hero__cta--primary">
-                                <a href="https://store.steampowered.com/app/1663040/Amber_Isle/" class="button button--primary button--expand">
-                                    Buy now on Steam
+                                <a href="https://stream-koolaid.github.io/Clock-Widget/" class="button button--primary button--expand">
+                                    Download Now
                                 </a>
                             </div>
                         </div>
@@ -30,17 +27,17 @@ function checkProductInfoAndUpdateCTA() {
 				}
 				break;
 
-			case moreinfoText.includes('AMBERISLESWITCH'):
+			case moreinfoText.includes('UPTIMEWIDGET'):
 				if (ctaElement) {
 					ctaElement.innerHTML = `
-                        <div class="hero__cta-container hero__cta-container--center">
-                            <div class="hero__cta hero__cta--primary">
-                                <a href="https://switch.link/" class="button button--primary button--expand">
-                                    Buy now on Nintendo eShop
-                                </a>
+                            <div class="hero__cta-container hero__cta-container--center">
+                                <div class="hero__cta hero__cta--primary">
+                                    <a href="https://stream-koolaid.github.io/Clock-Widget/" class="button button--primary button--expand">
+                                        Download Now
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    `;
+                        `;
 				}
 				break;
 
